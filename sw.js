153c28a312dd12a5d98c0926a98d2feb751ea361
denisgolovaha88-creator <denisgolovaha88@gmail.com>
2026-06-02 10:56:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,49 @@
+const CACHE = 'nostradamus-v2';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        './'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match('./'))
+  );
+});
+
 self.addEventListener('push', event => {
 
-const data = event.data
-? event.data.json()
-: {
-title:'Крипто Нострадамус',
-body:'Новое пророчество готово'
-};
+  const data = event.data
+    ? event.data.json()
+    : {
+        title: 'Крипто Нострадамус',
+        body: 'Новое пророчество готово'
+      };
 
-event.waitUntil(
-
-self.registration.showNotification(
-data.title,
-{
-body:data.body,
-icon:'icon-192.png',
-badge:'icon-192.png'
-}
-)
-
-);
+  event.waitUntil(
+    self.registration.showNotification(
+      data.title,
+      {
+        body: data.body,
+        icon: 'icon-192.png',
+        badge: 'icon-192.png'
+      }
+    )
+  );
 
 });
 
 self.addEventListener('notificationclick', event => {
 
-event.notification.close();
+  event.notification.close();
 
-event.waitUntil(
-
-clients.openWindow('./')
-
-);
+  event.waitUntil(
+    clients.openWindow('./')
+  );
 
 });
