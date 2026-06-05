38c4fa4e98e5d98524302c4ff700e0aec29f5364
denisgolovaha88-creator <denisgolovaha88@gmail.com>
2026-06-05 20:34:26 +0300

    Update events.js

--- api/events.js
+++ api/events.js
@@ -1,1 +1,30 @@
+export default async function handler(req, res) {
 
+const events = [];
+
+try {
+
+const now = new Date();
+
+events.push({
+date: now.toISOString().split('T')[0],
+icon:'🟣',
+title:'API работает',
+coins:'BTC ETH',
+impact:10,
+move:'0%',
+text:'Тест ответа сервера'
+});
+
+res.status(200).json(events);
+
+}
+catch(e){
+
+res.status(500).json({
+error:e.message
+});
+
+}
+
+}
